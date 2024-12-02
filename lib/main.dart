5bafb96ae436011cc7e977909ff93ffb6efc787f
Catarina Coelho <k.tarina56@gmail.com>
2024-12-02 13:46:23 -0300

    start design

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
-void main() => runApp(const PrimeiroApp());
+void main() => runApp(PrimeiroApp());
 class PrimeiroApp extends StatelessWidget {
   const PrimeiroApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Oi Flutter',
-        home: Home()
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.cyan,
+        hintColor: Colors.blue,
+      ),
+        home: Home(),
     );
   }
 }
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Lista de Contatos",
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      actions: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+               )
+            ],
+        )
+      ],
+      ),
+      body: Column(
+        children: <Widget>[
+          TopBar(),
+        ],
+      ),
+    );
   }
 }
